2c242b794ae9f5b82c87755d594f3115916e1f5d
Diego <diegofernades9506@gmail.com>
2020-01-10 17:40:13 -0300

    Feat: metodos para o calculo

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,11 +9,8 @@ import { ApiService } from '../services/api.service';
 export class HomeComponent {
 
   message: string;
-  calculate = {
-    state_id: '',
-    city_id: '',
-    consumption: ''
-  };
+  calculate: any ={};
+  result: any = [];
   states = [];
   citys = [];
   hsps = [];
@@ -36,12 +33,26 @@ export class HomeComponent {
   }
 
   public submit (calculate) {
+    let state = this.hsps.find(hsp => { return hsp.id == calculate.state_id });
+    this.result.consumptionPercentage = calculate.consumption * 1.20
+    this.result.kw = this.calculateKw(this.result.consumptionPercentage, state.goal) + ' kWp';
+    this.result.boards = this.calculateBoards(this.result.consumptionPercentage);
+    this.result.area = this.result.boards*2 + ' m²';
+    // this.result.month = this.result.boards*345;
+    // this.result.month = this.result.month + ' kWh';
+  }
 
+  private calculateBoards(consumption) {
+    return Math.round((consumption*12)/345)
+  }
+
+  private calculateKw (kw, goal) {
+    let kwResult = (kw/30)/goal
+    return kwResult.toFixed(2);
   }
 
   private getHsp() {
     this.hsps = this.apiService.getHsp();
-    console.log(this.hsps)
   }
 
   private getStates () {
